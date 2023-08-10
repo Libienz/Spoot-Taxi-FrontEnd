ebcaf9886222f436a0976ac68686bc1af8a966a0
Kuen Hee Lee <Leekh10195@gmail.com>
2023-08-11 07:49:34 +0900

    Fix: RallyFragment RecyclerView Rendering

--- app/src/main/java/com/example/spoot_taxi_front/fragments/RallyFragment.java
+++ app/src/main/java/com/example/spoot_taxi_front/fragments/RallyFragment.java
@@ -3,25 +3,29 @@ package com.example.spoot_taxi_front.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.spoot_taxi_front.R;
+import com.example.spoot_taxi_front.adapters.RallyAdapter;
 import com.example.spoot_taxi_front.network.api.RallyApi;
 import com.example.spoot_taxi_front.network.dto.RallyInformationDto;
 import com.example.spoot_taxi_front.network.dto.responses.RallyResponse;
 import com.example.spoot_taxi_front.network.retrofit.ApiManager;
 import com.example.spoot_taxi_front.utils.SessionManager;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -31,31 +35,44 @@ import retrofit2.Response;
 
 public class RallyFragment extends Fragment {
 
+    private RecyclerView rallyRecyclerView;
+    private RallyAdapter rallyAdapter;
+    private List<RallyInformationDto.RallyDetailsDto> rallyDetailsDtoList = new ArrayList<>();
     RallyApi rallyApi;
+    private TextView rallyDateTextView;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_rally, container, false);
-        Button updateButton = view.findViewById(R.id.button_update);
-
-
-        updateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("버튼눌림","눌렸음");
-                setRallyInformation();
-            }
-        });
-
+        rallyApi = ApiManager.getInstance().createRallyApi(SessionManager.getInstance().getJwtToken());
+        // 뷰 요소 초기화
+        rallyRecyclerView = view.findViewById(R.id.rallyRecyclerView);
+        rallyDateTextView = view.findViewById(R.id.rallyDateTextView);
+        rallyDateTextView.setText(LocalDateTime.now().getMonthValue() + "월 " + LocalDateTime.now().getDayOfMonth() + "일 주요집회");
+        rallyRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rallyAdapter = new RallyAdapter();
+        getRallyInformationFromServer();
+        rallyAdapter.setRallyDetails(rallyDetailsDtoList);
+        rallyRecyclerView.setAdapter(rallyAdapter);
         return view;
     }
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        Log.d("chatFragment", "onHiddenChanged: ");
+        if (hidden == false) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d("chatFragment", "ui update re");
+                    getRallyInformationFromServer();
+                }
+            });
 
+        }
+    }
 
-    private void setRallyInformation() {
-
-        //Api Client 생성
-        rallyApi = ApiManager.getInstance().createRallyApi(SessionManager.getInstance().getJwtToken());
+    private void getRallyInformationFromServer() {
 
         Call<RallyResponse> call = rallyApi.getRallyInfo();
 
@@ -79,8 +96,8 @@ public class RallyFragment extends Fragment {
         switch (statusCode) {
             case 200:
                 RallyInformationDto rallyInformationDto = responseBody.getRallyInformationDto();
-                setRallyInfo(rallyInformationDto);
-
+                rallyDetailsDtoList = rallyInformationDto.getRallyDetailsDtoList();
+                rallyAdapter.setRallyDetails(rallyDetailsDtoList);
                 break;
             default:
                 Toast.makeText(getContext(), "집회정보를 받아올수 없습니다.", Toast.LENGTH_SHORT).show();
@@ -89,55 +106,4 @@ public class RallyFragment extends Fragment {
         }
     }
 
-    private void setRallyInfo(RallyInformationDto responseBody) {
-        TableLayout tableLayout =getView().findViewById(R.id.rallyTL);
-        removeCurrentRally(tableLayout);
-
-        TextView date = getView().findViewById(R.id.rallyDateTv);
-        date.setText(responseBody.getDate().getMonthValue() + "월" + responseBody.getDate().getDayOfMonth() + "일");
-
-        List<RallyInformationDto.RallyDetailsDto> rallyDetailsList = responseBody.getRallyDetailsList();
-        for (RallyInformationDto.RallyDetailsDto details : rallyDetailsList) {
-            String time =details.getStartTime().getHour()+"시"+details.getStartTime().getMinute()+"분"+" ~ "+details.getEndTime().getHour()+"시"+details.getEndTime().getMinute()+"분";
-            addTableRow(tableLayout,time,details.getLocation(),details.getRallyScale(),details.getJurisdiction());
-
-        }
-    }
-
-    private static void removeCurrentRally(TableLayout tableLayout) {
-        int childCount = tableLayout.getChildCount();
-        if(childCount>3){
-            for(int i =childCount-1; i>=3; i--){
-                tableLayout.removeViewAt(i);
-            }
-        }
-    }
-
-    // Fragment 내에서 동적으로 TableRow를 추가하는 메소드
-    private void addTableRow(TableLayout tableLayout, String time, String location,String rallyAttendance,String policeStation) {
-        TableRow tableRow = new TableRow(getContext());
-
-        TextView textViewTime = createTextView(time);
-        TextView textViewLocation = createTextView(location);
-        TextView textViewRallyAttendance = createTextView(rallyAttendance);
-        TextView textViewPoliceStation = createTextView(policeStation);
-
-        // TableRow에 TextView 추가
-        tableRow.addView(textViewTime);
-        tableRow.addView(textViewLocation);
-        tableRow.addView(textViewRallyAttendance);
-        tableRow.addView(textViewPoliceStation);
-
-        // TableLayout에 TableRow 추가
-        tableLayout.addView(tableRow);
-    }
-    // TextView를 생성하고 스타일을 적용하는 메소드
-    private TextView createTextView(String text) {
-        TextView textView = new TextView(getContext());
-        textView.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.MATCH_PARENT, 1));
-        textView.setText(text);
-        textView.setGravity(Gravity.CENTER);
-        textView.setPadding(10, 10, 10, 10);
-        return textView;
-    }
 }
